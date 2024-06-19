c00de90cea76738d64a4a20131bedc558faf29da
Lone-Hassan <lone.hassan@gmail.com>
2024-06-19 15:29:08 +0500

    project for prcatice Jenkins

---
+++ src/Animal.java
@@ -0,0 +1,17 @@
+//import animals.*;
+/**
+ * Java doc comment
+ */
+public class Animal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println(args[0]+' '+args[1]);
+		System.out.println(args.length);
+	
+		animals an = new animals();
+		an.echo();
+
+	}
+
+}
---
+++ src/animals.java
@@ -0,0 +1,13 @@
+/**
+ * @author Hassan
+ * just to test
+ */
+public class animals {
+
+	/**
+	 * this msg just prints
+	 */
+	public void echo() {
+		System.out.println("executed!!");
+	}
+}
